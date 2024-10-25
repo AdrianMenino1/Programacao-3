9bed876af3de9e3b0c1ca3438792ccd896e16848
AdrianMenino1 <133375115+AdrianMenino1@users.noreply.github.com>
2024-10-25 15:57:07 -0300

    Add files via upload

---
+++ loiro/perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Vôleibol',
+    'Quantos pontos um time precisa para finalizar um set de voleibol?',
+    '25'
+)
+
+criaCartao(
+    'Vôleibol',
+    'Quantos jogadores jogam simultaneamente pelo mesmo time durante um ponto em uma partida de vôlei?',
+    '6'
+)
+
+criaCartao(
+    'Vôleibol',
+    'Qual ação realizada pelo jogador na posição 1 dá início a um ponto no vôleibol?',
+    'Saque'
+)
+
+criaCartao(
+    'Vôleibol',
+    'Dentre as posições do vôlei, qual delas costuma precisar de um jogador mais alto?',
+    'Central'
+)
+
+criaCartao(
+    'Vôleibol',
+    'Dentre as posições do vôlei, qual delas costuma precisar de um jogador canhoto?',
+    'Oposto'
+)
